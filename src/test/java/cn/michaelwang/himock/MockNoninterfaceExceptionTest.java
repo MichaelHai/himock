52d4f13c73fb1b2732adb300a5eb399bbb0e638d
Michael Wang <michael.wanghai@outlook.com>
2015-10-14 17:37:26 +0800

    merge the two test cases

--- src/test/java/cn/michaelwang/himock/MockNoninterfaceExceptionTest.java
+++ src/test/java/cn/michaelwang/himock/MockNoninterfaceExceptionTest.java
@@ -6,21 +6,12 @@ import static org.junit.Assert.assertEquals;
 
 public class MockNoninterfaceExceptionTest {
     @Test
-    public void testCannotMockClassExceptionShouldContainsMockedClassInformation() {
+    public void testCannotMockClassExceptionShouldContainsMockedClassInformationAndGenerateProperMessage() {
         HiMock mock = new HiMock();
         try {
             mock.mock(String.class);
         } catch (MockNoninterfaceException ex) {
             assertEquals("the exception should contains the failed mocked class information", String.class, ex.getMockedClass());
-        }
-    }
-
-    @Test
-    public void testCannotMockClassExceptionShouldGenerateProperMessage() {
-        HiMock mock = new HiMock();
-        try {
-            mock.mock(String.class);
-        } catch (MockNoninterfaceException ex) {
             assertEquals("java.lang.String is not an interface and can(should) not be mocked.", ex.getMessage());
         }
     }
