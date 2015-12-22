a701eb51e61f77f9761c7114cb386bdf6ba82df3
Hai Wang <michael.wanghai@outlook.com>
2015-12-22 21:20:31 +0800

    himock-12 ordered verification more tests

--- himock/src/test/java/cn/michaelwang/himock/OrderedVerificationTest.java
+++ himock/src/test/java/cn/michaelwang/himock/OrderedVerificationTest.java
@@ -1,5 +1,6 @@
 package cn.michaelwang.himock;
 
+import cn.michaelwang.himock.report.HiMockReporter;
 import org.junit.Test;
 
 @SuppressWarnings("CodeBlock2Expr")
@@ -43,5 +44,75 @@ public class OrderedVerificationTest extends HiMockBaseTest {
                 "\t\t\tcn.michaelwang.himock.MockedInterface.returnInt()\n" +
                 "\t\t\tcn.michaelwang.himock.MockedInterface.doNothing()\n" +
                 "\t\t\tcn.michaelwang.himock.MockedInterface.returnInt()\n");
+    }
+
+    @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+    @Test
+    public void testOrderWithOtherCallsCanBeVerified() {
+        MockedInterface dummy = mock.mock(MockedInterface.class);
+
+        dummy.returnInt();
+        dummy.returnObject();
+        dummy.returnBoolean();
+        dummy.returnUserException();
+
+        mock.verifyInOrder(() -> {
+            dummy.returnInt();
+            dummy.returnBoolean();
+        });
+    }
+
+    @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+    @Test
+    public void testWithOtherCallsNotOrderedShouldFail() {
+        MockedInterface dummy = mock.mock(MockedInterface.class);
+
+        dummy.returnInt();
+        dummy.returnObject();
+        dummy.returnBoolean();
+        dummy.returnUserException();
+
+        reportTest(() -> {
+            mock.verifyInOrder(() -> {
+                dummy.returnBoolean();
+                dummy.returnInt();
+            });
+        }, "Verification failed:\n" +
+                "\tmethod called in different order:\n" +
+                "\t\tverified order:\n" +
+                "\t\t\tcn.michaelwang.himock.MockedInterface.returnBoolean()\n" +
+                "\t\t\tcn.michaelwang.himock.MockedInterface.returnInt()\n" +
+                "\t\tactual order:\n" +
+                "\t\t\tcn.michaelwang.himock.MockedInterface.returnInt()\n" +
+                "\t\t\tcn.michaelwang.himock.MockedInterface.returnBoolean()\n");
+    }
+
+    @Test
+    public void testDifferentMockedObjectCanBeVerifiedWithOrder() {
+        MockedInterface dummy1 = mock.mock(MockedInterface.class);
+        MockedInterface dummy2 = mock.mock(MockedInterface.class);
+
+        dummy1.doNothing();
+        dummy2.doNothing();
+
+        mock.verifyInOrder(() -> {
+            dummy1.doNothing();
+            dummy2.doNothing();
+        });
+    }
+
+
+    @Test(expected = HiMockReporter.class)
+    public void testDifferentMockedObjectNotOrderedShouldFail() {
+        MockedInterface dummy1 = mock.mock(MockedInterface.class);
+        MockedInterface dummy2 = mock.mock(MockedInterface.class);
+
+        dummy1.doNothing();
+        dummy2.doNothing();
+
+        mock.verifyInOrder(() -> {
+            dummy2.doNothing();
+            dummy1.doNothing();
+        });
     }
 }
