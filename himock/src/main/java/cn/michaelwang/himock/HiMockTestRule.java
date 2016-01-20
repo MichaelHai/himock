fd4f2cdb5d445fefcd699977fca836ba99a39504
Hai Wang <michael.wanghai@outlook.com>
2016-01-20 22:22:30 +0800

    himock-21 annotations and test rules of junit

---
+++ himock/src/main/java/cn/michaelwang/himock/HiMockTestRule.java
@@ -0,0 +1,18 @@
+package cn.michaelwang.himock;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class HiMockTestRule implements TestRule {
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                HiMock.setup();
+                statement.evaluate();
+            }
+        };
+    }
+}
