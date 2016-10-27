ae3235a656715777fda0160556927a485274fc76
Hai Wang <michael.wanghai@outlook.com>
2016-10-27 14:09:57 +0800

    remove the no use exceptions

--- himock/src/main/java/cn/michaelwang/himock/preprocess/RetrieveMatcherForNonMatcherArgException.java
+++
@@ -1,9 +0,0 @@
-package cn.michaelwang.himock.preprocess;
-
-public class RetrieveMatcherForNonMatcherArgException extends RuntimeException {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2030842566300312372L;
-
-}
