1a52be5c9a9cb9e24afe6948ad85042a16492ddf
Hai Wang <michael.wanghai@outlook.com>
2016-11-22 11:37:09 +0800

    use lambda replace the anonymous class.

--- himock/src/main/java/cn/michaelwang/himock/process/mockup/InterfaceMockBuilder.java
+++ himock/src/main/java/cn/michaelwang/himock/process/mockup/InterfaceMockBuilder.java
@@ -14,24 +14,18 @@ public class InterfaceMockBuilder<T> extends BaseInvocationBuilder<T> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public T createMock(int id) {
-		// thi can not convert to lambda or else effect the line number retrieved in InvocationImpl.
-		//noinspection Convert2Lambda
 		return ((T) Proxy.newProxyInstance(
 				mockedType.getClassLoader(),
 				new Class<?>[] { mockedType },
-				new java.lang.reflect.InvocationHandler() {
-					@Override
-					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						int lineNumber = Utils.getLineNumberInTestSuit(testSuit);
-						InvocationImpl invocation = InvocationFactory.getInstance().create(id, method, args, lineNumber);
-						try {
-							return invocationListener.methodCalled(invocation);
-						} catch (NoExpectedInvocationException ex) {
-							return Utils.nullValue(invocation.getReturnType());
-						}
-					}
-
-				}));
+				(proxy, method, args) -> {
+                    int lineNumber = Utils.getLineNumberInTestSuit(testSuit);
+                    InvocationImpl invocation = InvocationFactory.getInstance().create(id, method, args, lineNumber);
+                    try {
+                        return invocationListener.methodCalled(invocation);
+                    } catch (NoExpectedInvocationException ex) {
+                        return Utils.nullValue(invocation.getReturnType());
+                    }
+                }));
 	}
 
 }
